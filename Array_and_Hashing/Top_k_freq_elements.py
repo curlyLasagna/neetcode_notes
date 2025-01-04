7922254cc408b487eaa70c98c167416a81b57d16
Luis Dale <luis.gcodes@gmail.com>
2025-01-04 17:08:28 -0500

    understand top k freq

--- Array_and_Hashing/Top_k_freq_elements.py
+++ Array_and_Hashing/Top_k_freq_elements.py
@@ -1,14 +1,10 @@
 from typing import Counter, List
+import heapq as hq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        if len(nums) == k:
+                    return nums
         myHash = Counter(nums)
-        myRes = []
-        max_freq = 0
-        for i in range(k):
-            max_freq = max(myHash, key=myHash.get)
-            myRes.append(max_freq)
-            del myHash[max_freq]
-            print(myHash)
-        return myRes
-
+        return hq.nlargest(k, myHash.keys(), key = myHash.get)
+        
 print(Solution().topKFrequent([1,1,1,2,2,3], k = 2))
